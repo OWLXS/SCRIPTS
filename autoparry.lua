497d6d1b6b1a7b5015cc170505c96251ec076637
Mommy <123270567+OWLXS@users.noreply.github.com>
2024-02-01 02:12:18 -0300

    Update autoparry.lua

--- autoparry.lua
+++ autoparry.lua
@@ -1,10 +1,14 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
+local Remotes = ReplicatedStorage:WaitForChild("Remotes", 9e9)
+local Balls = workspace:WaitForChild("Balls", 9e9)
 local typeof = typeof
 local os = os
 local Player = Players.LocalPlayer or Players.PlayerAdded:Wait()
-local Remotes = ReplicatedStorage:WaitForChild("Remotes")
-local Balls = workspace:WaitForChild("Balls")
+
+--Bypass
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OWLXS/partytime/main/bypass.lua"))()
+warn("Security was destroyed...")
 
 local function VerifyBall(Ball)
     return typeof(Ball) == "Instance" and Ball:IsA("BasePart") and Ball:IsDescendantOf(Balls) and Ball:GetAttribute("realBall") == true
@@ -23,24 +27,26 @@ Balls.ChildAdded:Connect(function(Ball)
         return
     end
     
+    local minDistance = 15
+    local parryWindow = 0.7
+
     local OldPosition = Ball.Position
     local OldClock = os.clock()
-    
+
     Ball:GetPropertyChangedSignal("Position"):Connect(function()
         if IsTarget() then
             local PlayerPos = Player.Character.HumanoidRootPart.Position
             local Distance = (Ball.Position - PlayerPos).Magnitude
             local Velocity = (OldPosition - Ball.Position).Magnitude
-            if (Distance / Velocity) <= 20 then
+            
+            local timeToCollision = Distance / Velocity
+            if timeToCollision <= minDistance and (os.clock() - OldClock) <= parryWindow then
                 Parry()
             end
         end
-        
-        if (os.clock() - OldClock >= 1/60) then
-            OldClock = os.clock()
-            OldPosition = Ball.Position
-        end
+        OldClock = os.clock()
+        OldPosition = Ball.Position
     end)
 end)
 
-warn("I'm here mommy.")
+print("I'm here mommy.")
